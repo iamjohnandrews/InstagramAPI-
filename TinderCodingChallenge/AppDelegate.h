82eb4f9b7e6a4a3265a19414c25492cb3e105a52
iamjohnandrews <johnnydrews@gmail.com>
2016-02-08 20:53:38 -0500

    first commit

---
+++ TinderCodingChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TinderCodingChallenge
+//
+//  Created by John Andrews on 2/8/16.
+//  Copyright © 2016 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TinderCodingChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TinderCodingChallenge
+//
+//  Created by John Andrews on 2/8/16.
+//  Copyright © 2016 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
